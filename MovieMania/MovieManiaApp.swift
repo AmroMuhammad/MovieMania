e5392a511ce69b964587be1e04420e580e8c125a
Amr Muhammad <amrmu94@gmail.com>
2026-04-10 18:30:22 +0200

    Initial Commit

---
+++ MovieMania/MovieManiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieManiaApp.swift
+//  MovieMania
+//
+//  Created by Amr Muhammad on 10/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct MovieManiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MovieManiaTests/MovieManiaTests.swift
@@ -0,0 +1,17 @@
+//
+//  MovieManiaTests.swift
+//  MovieMania
+//
+//  Created by Amr Muhammad on 10/04/2026.
+//
+
+import Testing
+@testable import MovieMania
+
+struct MovieManiaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
